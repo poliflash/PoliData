fd8570d4b7d2fe7641e52875d83eb6b8d7d70cff
Poli Flash <poliflash@gmail.com>
2020-01-07 18:27:36 -0300

    Updates

---
+++ precache-manifest.ea9dfe355360d29c67bc24639fa9c8ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e535593a869289fe37433f0a6041f0c6",
+    "url": "/PoliData/index.html"
+  },
+  {
+    "revision": "f18537fe958eb73834f1",
+    "url": "/PoliData/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d17a6f4b90110c7dcc8c",
+    "url": "/PoliData/static/js/2.f21d2b76.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/PoliData/static/js/2.f21d2b76.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f18537fe958eb73834f1",
+    "url": "/PoliData/static/js/main.422eae7f.chunk.js"
+  },
+  {
+    "revision": "b82d7d9d49dd850dec2a",
+    "url": "/PoliData/static/js/runtime-main.169e2d52.js"
+  }
+]);
